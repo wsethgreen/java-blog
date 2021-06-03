e96b19e60051b63ab1b8d817a2c3aac0f34ff510
wsethgreen <wsethgreen@gmail.com>
2021-06-03 12:01:11 -0400

    adding additional functionality

--- src/main/java/com/TechTalent/BlogOfTalentTech/Controller/BlogPostController.java
+++ src/main/java/com/TechTalent/BlogOfTalentTech/Controller/BlogPostController.java
@@ -4,29 +4,47 @@ import com.TechTalent.BlogOfTalentTech.Model.BlogPost;
 import com.TechTalent.BlogOfTalentTech.Repository.BlogPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BlogPostController {
 
     @Autowired
     private BlogPostRepository blogPostRepository;
+    private static List<BlogPost> posts = new ArrayList<>();
 
     @GetMapping(value = "/")
-    public String index(BlogPost blogPost) {
+    public String index(BlogPost blogPost, Model model) {
+        model.addAttribute("posts", posts);
         return "blogPost/index";
     }
 
     private BlogPost blogPost;
 
-    @PostMapping(value = "/")
+    @PostMapping(value = "/blogposts")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
-        blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getBlogEntry(), blogPost.getAuthor()));
+//        blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getBlogEntry(), blogPost.getAuthor()));
+        blogPostRepository.save(blogPost);
+        posts.add(blogPost);
         model.addAttribute("title", blogPost.getTitle());
         model.addAttribute("author", blogPost.getAuthor());
         model.addAttribute("blogEntry", blogPost.getBlogEntry());
         return "blogPost/result";
     }
+
+    @GetMapping(value = "/blogposts/new")
+    public String newBlog(BlogPost blogPost) {
+        return "blogPost/newBlogPost";
+    }
+
+    @DeleteMapping(value = "/blogposts/{id}")
+    public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
+        blogPostRepository.deleteById(id);
+        return "blogPost/index";
+    }
+
 }
